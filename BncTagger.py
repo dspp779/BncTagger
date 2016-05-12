5b5004cd7eb04b559159053cc79a283bfa47c8e4
JJC <jjc@nlplab.cc>
2016-05-12 18:26:06 +0800

    pep8 coding style

--- BncTagger.py
+++ BncTagger.py
@@ -3,13 +3,16 @@ import sqlite3
 from itertools import izip
 from collections import defaultdict
 
-TABLE_SCHEMA = ('CREATE TABLE WordLemma('
-                    "word TEXT COLLATE NOCASE, "
-                    'lemma TEXT, '
-                    'tag TEXT, '
-                    'probability REAL, '
-                    'PRIMARY KEY (word, lemma, tag)'
-                    ');')
+TABLE_SCHEMA = (
+    'CREATE TABLE WordLemma('
+    'word TEXT COLLATE NOCASE, '
+    'lemma TEXT, '
+    'tag TEXT, '
+    'probability REAL, '
+    'PRIMARY KEY (word, lemma, tag)'
+    ');'
+)
+
 
 def parse_bnc_word_lemma():
     for line in fileinput.input('bnc.word.lemma.pos.txt'):
@@ -17,8 +20,10 @@ def parse_bnc_word_lemma():
         # data parsing
         lemma, word, tag, count, total, prob = lemma[1:-1], word[1:-1], tag[1:-1], float(count), float(total), float(prob)
         # ignore foreign words (tag = F)
-        if tag == 'F': continue
+        if tag == 'F':
+            continue
         yield (word, lemma, tag, prob)
+
 
 def init_word_lemma_db(words):
     with get_connection() as conn:
@@ -29,11 +34,13 @@ def init_word_lemma_db(words):
         # insert data
         cur.executemany('INSERT INTO WordLemma VALUES (?, ?, ?, ?)', words)
 
+
 def get_connection(db_path='bnc_word_lemma_pos.db'):
     conn = sqlite3.connect(db_path)
     # object type returned for TEXT data type
     conn.text_factory = str
     return conn
+
 
 def search_lemma(word):
     with get_connection() as conn:
@@ -42,6 +49,7 @@ def search_lemma(word):
         for res in cur.execute(cmd, (word,)):
             return res[0]
 
+
 def search_tag(word):
     with get_connection() as conn:
         cur = conn.cursor()
@@ -49,30 +57,48 @@ def search_tag(word):
         for res in cur.execute(cmd, (word,)):
             return res[0]
 
+
 def bncTag(words):
-    tagged = [ str(search_tag(w.strip().lower())) for w in words ]
+    tagged = [str(search_tag(w.strip().lower())) for w in words]
     return tagged
+
 
 class BncTagger:
     def __init__(self, db_path='bnc_word_lemma_pos.db'):
         self.conn = get_connection(db_path)
+
     def __del__(self):
         self.conn.close()
+
     def __getitem__(self, key):
         return self.search_tag(key)
+
     def search_tag(self, word):
         cur = self.conn.cursor()
-        cmd = 'SELECT tag, MAX(probability) FROM WordLemma WHERE word=? GROUP BY word;'
+        cmd = (
+            'SELECT tag, MAX(probability) '
+            'FROM WordLemma '
+            'WHERE word=? '
+            'GROUP BY word;'
+        )
         cur.execute(cmd, (word,))
         return cur.fetchone()
+
     def parse(self, words):
         cur = self.conn.cursor()
         # search tags for all words
-        cmd = 'SELECT word, tag, MAX(probability) FROM WordLemma WHERE word in ({0}) GROUP BY word;'.format(', '.join('?'*len(words)))
+        cmd = (
+            'SELECT word, tag, MAX(probability) '
+            'FROM WordLemma '
+            'WHERE word in ({0}) '
+            'GROUP BY word;'
+        ).format(', '.join('?'*len(words)))
+
         pos_dict = defaultdict(lambda: 'None')
-        pos_dict.update( (word.lower(), pos) for word, pos, prob in cur.execute(cmd, words) )
+        pos_dict.update((word.lower(), pos) for word, pos, prob in cur.execute(cmd, words))
         tagged = tuple(pos_dict[word.lower()] for word in words)
         return tagged
+
 
 if __name__ == '__main__':
     # insert data into sqlite3 db
